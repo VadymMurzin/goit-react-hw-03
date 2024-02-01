c85efd5dc73002f1fe70c50c1903b23493a938e4
VadymMurzin <prometey736@gmail.com>
2024-02-01 21:11:38 +0100

    Add React icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,7 @@
 import css from './contact.module.css'
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { IoMdPerson } from "react-icons/io";
+
 
 export default function Contact ({ contact, onDeleteContact }) {
   const { id, name, number } = contact;
@@ -7,8 +10,8 @@ export default function Contact ({ contact, onDeleteContact }) {
   };
   return (
     <li className={css.liStyle}>
-      <p className={css.pStyle}>Name: {name}</p>
-      <p className={css.pStyle}>Number: {number}</p>
+      <p className={css.pStyle}><BsFillTelephoneFill className={css.iconStyle}/>{name}</p>
+      <p className={css.pStyle}><IoMdPerson className={css.iconStyle}/>{number}</p>
       <button className={css.btnStyle} onClick={handleDeleteClick}>Delete</button>
     </li>
   );
